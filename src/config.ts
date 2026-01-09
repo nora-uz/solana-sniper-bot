197f0ed996499fcc8848f0c779bf4a6ac1ad2ae1
Ai.Pro <jakh.akhmedov@gmail.com>
2026-01-09 16:04:27 +0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ dotenv.config();
 export const config = {
   heliusApiKey: process.env.HELIUS_API_KEY!,
   rpcEndpoint: process.env.RPC_ENDPOINT!,
-  walletPrivateKey: process.env.WALLET_PRIVATE_KEY!,
+  walletPrivateKey: process.env.WALLET_PRIVATE_KEY!
 };
